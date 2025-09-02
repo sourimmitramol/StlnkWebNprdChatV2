7a3e98c7a63ba9835cb4505994aea27f8a016ba4
sourimmitramol <sourim.mitra@molgroup.com>
2025-09-02 21:42:15 +0530

    Update router.py

--- agents/router.py
+++ agents/router.py
@@ -29,7 +29,11 @@ def route_query(query: str) -> str:
         return get_container_carrier(query)
     elif ("arrived" in q or "reached" in q or "arrival" in q or "on water" in q or "on the water" in q) and ("container" in q or "po" in q or any(char.isdigit() for char in q)):
         return check_arrival_status(query)
-    elif any("milestone", "status", "track", "event history", "where is") in q:
+    #elif any("milestone", "status", "track", "event history", "where is") in q:
+        #return get_container_milestones(query)
+    elif "milestone or status" in q:
+        return get_container_milestones(query)
+    elif "milestone or status or where or track" in q and ("container" in q or any(char.isdigit() for char in q)):
         return get_container_milestones(query)
     elif "delay" in q:
         return get_delayed_containers(query)
@@ -72,6 +76,7 @@ def route_query(query: str) -> str:
 # from agents.router import route_query
 
 # result = route_query(user_query)
+
 
 
 
