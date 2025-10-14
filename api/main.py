4d00b658ef73ecd9259d3fcfa951a9a5f745e5cc
sourimmitramol <sourim.mitra@molgroup.com>
2025-10-14 16:43:34 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -93,7 +93,9 @@ def check_container_authorization(df, container_numbers: List[str], consignee_co
 @app.post("/ask")
 def ask(body: QueryWithConsigneeBody):
     q = body.question.strip()
-    consignee_codes = [c.strip() for c in body.consignee_code.split(",") if c.strip()]
+    #consignee_codes = [c.strip() for c in body.consignee_code.split(",") if c.strip()]
+    consignee_codes = list(dict.fromkeys(c.strip() for c in body.consignee_code.split(",") if c.strip()))
+
     print(consignee_codes)
     if not q:
         raise HTTPException(status_code=400, detail="Empty question")
@@ -316,6 +318,7 @@ def ask(body: QueryWithConsigneeBody):
         except Exception as fallback_exc:
             logger.error(f"Router fallback also failed: {fallback_exc}")
             raise HTTPException(status_code=500, detail=f"Agent failed: {exc}")
+
 
 
 
